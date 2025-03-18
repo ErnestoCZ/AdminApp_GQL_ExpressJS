5d235b3d6923f32feb76d5080a286d9e6b852ae9
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-03-18 20:42:17 +0100

    added getUsers query in schema and its implementation in resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,15 +1,49 @@
-import { Resolvers, User } from "./generated/schema"
+import { Resolvers , User} from "./generated/schema"
+import AppDataSource from "./database/DataSource";
+import { UserEntity } from "./database/Entities/User.entity";
+import { GraphQLError } from "graphql";
+
+const UserRepository = AppDataSource.getRepository(UserEntity)
 
 
 export const resolvers : Resolvers  = {
     Query: {
+        getUsers: async (parent) : Promise<UserEntity[]> => {
+            return await UserRepository.createQueryBuilder().select('user').from(UserEntity,'user').getMany();
+        }
 
     },
     Mutation:{
-        createUser: (parent, { userData }): User => {
-            console.log(parent)
-            return {id:'1', firstName: userData.firstName, lastName:userData.lastName, email:userData.email };
+        createUser: async (parent, { userData }): Promise<UserEntity> => {
+            
+            const findUser = UserRepository.findOneBy({email: userData.email});
+            const createUser = UserRepository.create(userData);
+
+            const [foundUser, createdUser] = await Promise.all([findUser,createUser]);
+
+            if(!foundUser){
+                return UserRepository.save(createUser);
+            }
+            else{
+                throw new GraphQLError("User already Exists");
+            }
         },
+        // deleteUser: async (parent, {userID}) : Promise<> => {
+
+        //     const user = UserRepository.findOneBy({id :userID.id});
+        //     const [foundUser] = await Promise.all([user]);
+
+
+        //     if(!foundUser){
+        //         throw new GraphQLError('User not found');
+        //     }
+        //     else{
+        //         return await UserRepository.createQueryBuilder().delete().from(UserEntity).where("id = :id", {id: userID.id}).execute();
+        //     }
+
+        //     return {} as UserEntity;
+        // }
+
 
     }
 }
