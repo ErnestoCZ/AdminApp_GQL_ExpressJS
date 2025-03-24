97a02ab41454f569d2e79446f624eeec9ed6c3a4
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-03-24 21:24:14 +0100

    added getter queries for all existing entities

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,26 +1,70 @@
-import { Resolvers , User} from "./generated/schema"
+import { Resolvers , UserList , User as GQLUser, BillingList} from "./generated/schema"
 import AppDataSource from "./database/DataSource";
-import { UserEntity } from "./database/Entities/User.entity";
+import { User } from "./database/Entities/User.entity";
 import { GraphQLError } from "graphql";
+import { Billing } from "./database/Entities/Billing.entity";
+import { SelectQueryBuilder } from "typeorm";
+import { BillingItem } from "./database/Entities/BillingItem.entity";
 
-const UserRepository = AppDataSource.getRepository(UserEntity)
-
+const UserRepository = AppDataSource.getRepository(User)
+const BillingRepository = AppDataSource.getRepository(Billing)
+const BillingItemRepository = AppDataSource.getRepository(BillingItem)
 
 export const resolvers : Resolvers  = {
     Query: {
-        getUsers: async (parent) : Promise<UserEntity[]> => {
-            return await UserRepository.createQueryBuilder().select('user').from(UserEntity,'user').getMany();
+        testQuery: async (parent, {limit}) => {
+            const userQuery = AppDataSource.createQueryBuilder().select('*').from(User, 'user');
+            if(limit){
+                userQuery.limit(limit);
+            }
+            return await userQuery.execute();
+
+        },
+        getUsers: async (parent, {limit,offset}) => {
+            const itemsQuery =  UserRepository.createQueryBuilder().select('user').from(User,'user');
+            const totalCountQuery =  UserRepository.createQueryBuilder().select('user').getCount();
+            
+            if(limit){
+                itemsQuery.limit(limit)
+            }
+            if(offset){
+                itemsQuery.offset(offset)
+            }
+
+            const [items, totalCount] = await Promise.all([itemsQuery.execute(), totalCountQuery])
+            return {items, totalCount};
+        },
+        getBillings: async (parent, {limit,offset}) => {
+            const itemsQuery = BillingRepository.createQueryBuilder().select('billing').from(Billing, 'billing')
+            const totalCountQuery = BillingRepository.createQueryBuilder().select('billing').getCount();
+
+            if(limit){
+                itemsQuery.limit(limit)
+            }
+            if(offset){
+                itemsQuery.offset(offset)
+            }
+
+            const [items, totalCount] = await Promise.all([itemsQuery.execute(),totalCountQuery]);
+            return {items,totalCount};
+        },
+        getBillingItems: async (parent) => {
+
+            const query = await AppDataSource.createQueryBuilder().leftJoin('billingitem.user', 'user').from(BillingItem,'billingitem').getMany()
+            console.log(query)
+            return query;
+
         }
+        
+
+
 
     },
     Mutation:{
-        createUser: async (parent, { userData }): Promise<UserEntity> => {
-            
+        createUser: async (parent, { userData }): Promise<User> => {
             const findUser = UserRepository.findOneBy({email: userData.email});
             const createUser = UserRepository.create(userData);
-
             const [foundUser, createdUser] = await Promise.all([findUser,createUser]);
-
             if(!foundUser){
                 return UserRepository.save(createUser);
             }
@@ -28,7 +72,8 @@ export const resolvers : Resolvers  = {
                 throw new GraphQLError("User already Exists");
             }
         },
-        // deleteUser: async (parent, {userID}) : Promise<> => {
+
+        // deleteUser: async (parent, {userID})  => {
 
         //     const user = UserRepository.findOneBy({id :userID.id});
         //     const [foundUser] = await Promise.all([user]);
@@ -41,7 +86,7 @@ export const resolvers : Resolvers  = {
         //         return await UserRepository.createQueryBuilder().delete().from(UserEntity).where("id = :id", {id: userID.id}).execute();
         //     }
 
-        //     return {} as UserEntity;
+        //     return {} as User;
         // }
 
 
