4e1519c07a6e5aa23433b942883135e996a8216b
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-04-03 10:58:22 +0200

    added Constant entity to datasource

--- src/database/DataSource.ts
+++ src/database/DataSource.ts
@@ -1,21 +1,20 @@
-import {DataSource, DataSourceOptions} from 'typeorm'
+import { DataSource } from "typeorm";
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
-import { User } from './Entities/User.entity';
-import { BillingItem } from './Entities/BillingItem.entity';
-import { Billing } from './Entities/Billing.entity';
+import { User } from "./Entities/User.entity";
+import { BillingItem } from "./Entities/BillingItem.entity";
+import { Billing } from "./Entities/Billing.entity";
+import { Constant } from "./Entities/Constant.entity";
 
-
-const AppDataSource : DataSource = new DataSource({
-    host: 'localhost',
-    port: 5432,
-    type: 'postgres',
-    username : 'root',
-    password: '1234',
-    database: 'postgres',
-    entities: [User, BillingItem, Billing],
-    migrations: [__dirname + "/**/migrations/*{.js,.ts}"],
-    synchronize: true
-} as PostgresConnectionOptions)
-
+const AppDataSource: DataSource = new DataSource({
+  host: "localhost",
+  port: 5432,
+  type: "postgres",
+  username: "root",
+  password: "1234",
+  database: "postgres",
+  entities: [User, BillingItem, Billing, Constant],
+  migrations: [__dirname + "/**/migrations/*{.js,.ts}"],
+  synchronize: true,
+} as PostgresConnectionOptions);
 
 export default AppDataSource;
