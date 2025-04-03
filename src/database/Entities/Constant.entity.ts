fe4dbecffacc27c05181a8743d222e114cdaee77
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-04-03 11:02:37 +0200

    added constant entity for database

---
+++ src/database/Entities/Constant.entity.ts
@@ -0,0 +1,27 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+} from "typeorm";
+import { CONSTANT_TYPE } from "../types";
+
+@Entity()
+export class Constant {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @Column({
+    type: "enum",
+    enum: CONSTANT_TYPE,
+    nullable: false,
+    default: CONSTANT_TYPE.MEDIUM,
+  })
+  type!: CONSTANT_TYPE;
+
+  @Column("float", { nullable: false })
+  value!: number;
+}
