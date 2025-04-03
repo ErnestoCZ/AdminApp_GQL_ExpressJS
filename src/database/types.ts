f0bc7a0f78e1148dab657232f06330d14dbb413c
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-04-03 10:58:59 +0200

    added CONSTANT_TYPE enum

--- src/database/types.ts
+++ src/database/types.ts
@@ -1,4 +1,10 @@
 export enum STATUS {
-    ACTIVE = 'ACTIVE',
-    INACTIVE = "INACTIVE",
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
+
+export enum CONSTANT_TYPE {
+  LOW = "LOW",
+  MEDIUM = "MEDIUM",
+  HIGH = "HIGH",
 }
